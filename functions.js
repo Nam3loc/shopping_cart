4f320c89db004345f8200a5d25bc236ae27b5d5f
Nam3loc <colemanfullstackdev@gmail.com>
2023-05-23 17:06:08 -0400

    added delete functionality. trying to add a seperate delete function

--- functions.js
+++ functions.js
@@ -1,15 +1,44 @@
 // Imports
-import { inputField, listOfIngredients } from "./script.js";
+import { inputField, listOfIngredients, database } from "./script.js";
+import { ref, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 export const clearInputField = () => {
     return inputField.value = '';
 }
 
-export const addItem = (inputValue) => {
-    return listOfIngredients.innerHTML += `<li>${inputValue}</li>`;
-    // console.log(`${inputValue} added to the DB`);
+export const addItem = (currentIngredient) => {
+    let itemID = currentIngredient[0];
+    let itemName = currentIngredient[1];
+    
+    let newListElement = document.createElement('li');
+    newListElement.textContent = itemName;
+    listOfIngredients.append(newListElement);
+
+    newListElement.addEventListener('click', (evt) => {
+        // console.log('test', itemID);
+        let exactLocationInDB = ref(database, `ingredients/${itemID}`)
+        // console.log('test location', exactLocationInDB);
+        remove(exactLocationInDB);
+    })
 }
 
 export const clearIngredientList = () => {
     return listOfIngredients.innerHTML = '';
 }
+
+// export const deleteIngredient = (currentIngredient, evt) => {
+//     let itemID = currentIngredient[0];
+//     let itemName = currentIngredient[1];
+
+//     console.log('test id', itemID);
+//     console.log('test name', itemName);
+//     let exactLocationInDB = ref(database, `ingredients/${itemID}`)
+//     console.log('test location', exactLocationInDB);
+
+//     evt.target.addEventListener('dblclick', (evt) => {
+//         console.log('test', itemID);
+//         let exactLocationInDB = ref(database, `ingredients/${itemID}`)
+//         console.log('test location', exactLocationInDB);
+//         remove(exactLocationInDB);
+//     })
+// }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Imports
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 import { clearInputField, addItem, clearIngredientList } from './functions.js';
 
 // Defining elements
@@ -16,20 +16,29 @@ const appSettings = {
     databaseURL: "https://shopping-cart-2bc6e-default-rtdb.firebaseio.com/"
 }
 const app = initializeApp(appSettings);
-const database = getDatabase(app);
+export const database = getDatabase(app);
 const ingredientsInDB = ref(database, 'ingredients')
 
 // Database fetch function
 onValue(ingredientsInDB, function(snapshot) {
-    let ingredientsArray = Object.values(snapshot.val());
+    let ingredientsArray = Object.entries(snapshot.val());
+    // let ingredientsID = Object.keys(snapshot.val());
+
+    //console.log(ingredientsID);
 
     // Clear list so that there are no duplicates
     clearIngredientList();
 
     for(let i = 0; i < ingredientsArray.length; i++) {
         let currentIngredient = ingredientsArray[i];
+        let currentIngredientID = currentIngredient[0];
+        let currentIngredientName = currentIngredient[1];
+        // console.log('current id: ', currentIngredientID);
+        // console.log('current title: ', currentIngredientName);
+
 
         addItem(currentIngredient);
+        // deleteIngredient(currentIngredient);
     }
 })
 
@@ -41,9 +50,8 @@ addToCartButton.addEventListener('click', (evt) => {
     push(ingredientsInDB, inputValue);
 
     clearInputField();
-    // addItem(inputValue);
 })
 
-// https://www.youtube.com/watch?v=UFD4SP91tSM
-// 54:15
 // https://console.firebase.google.com/u/0/project/shopping-cart-2bc6e/database/shopping-cart-2bc6e-default-rtdb/data
+// https://www.youtube.com/watch?v=UFD4SP91tSM
+// 1:24:50
